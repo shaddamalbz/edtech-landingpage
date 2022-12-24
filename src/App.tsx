9e836ae4529c0812aca9f095e3ad6101c8c9457c
Shaddam Alghafiqih <shaddam.albz@gmail.com>
2022-12-24 12:41:24 +0700

    chore: setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-  return <h1>React + Vite with Airbnb style guide</h1>
+  return (
+    <section className="text-center text-2xl">
+      <h1>React + Vite with Airbnb style guide</h1>
+      <p>
+        integrated with
+        <span className="font-bold text-indigo-600"> Tailwind</span>
+      </p>
+    </section>
+  )
 }
 
 export default App
