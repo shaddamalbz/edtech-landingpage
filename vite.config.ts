f806ed4edd1b2268420617d13fafd48fdace7034
Shaddam Alghafiqih <shaddam.albz@gmail.com>
2023-01-01 11:57:29 +0700

    fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<repo>/',
+  base: 'https://shaddamalbz.github.io/edtech-landingpage/',
   plugins: [react()],
   test: {
     globals: true,
