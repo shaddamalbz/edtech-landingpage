ad7eca59512eabcf23c225fda977d07459a0c74c
Shaddam Alghafiqih <shaddam.albz@gmail.com>
2023-01-01 12:06:51 +0700

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://shaddamalbz.github.io/edtech-landingpage/',
+  base: '/edtech-landingpage/',
   plugins: [react()],
   test: {
     globals: true,
